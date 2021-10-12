1d8c605ffa13fbd5e48b7f1f28c773797a037add
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-12 22:53:48 +0330

    react-markdown@5 works

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import PostHeader from './PostHeader';
 
 import ReactMarkdown from 'react-markdown';
@@ -14,10 +15,26 @@ const PostContent: React.FC<Props> = ({ post }) => {
 
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customRenderers = {
+    // ! react-markdown will call this method, if it fiends an image in markdown content.
+    image(image: HTMLImageElement) {
+      console.log('from react-markdown', image.src);
+
+      return (
+        <Image
+          src={`/images/posts/${slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown children={content} renderers={customRenderers} />
     </article>
   );
 };

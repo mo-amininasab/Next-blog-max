82f541ce83e656f50b558bf901397406024f4d8c
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2021-10-13 02:39:41 +0330

    env vars update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    mongodb_username: 'mohammad',
-    mongodb_password: 'b7UpEq1WlYMFvcjU',
-    mongodb_clustername: 'cluster0',
-    mongodb_database: 'myBlog',
-  },
+
+}
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'mohammad',
+        mongodb_password: 'b7UpEq1WlYMFvcjU',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'myBlog-dev',
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'mohammad',
+      mongodb_password: 'b7UpEq1WlYMFvcjU',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'myBlog',
+    },
+  };
 };
